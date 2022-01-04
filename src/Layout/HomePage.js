183290ae9633041fd356394e024b80cf7472d858
Tony Tran <tran.tony.08@gmail.com>
2022-01-04 10:27:33 -0800

    Updated resume, projects list, added full stack project list

--- src/Layout/HomePage.js
+++ src/Layout/HomePage.js
@@ -5,13 +5,9 @@ import "../App.css";
 import corgibutt from "../imgs/corgibutt.jfif";
 import homegif from "../imgs/homepage.gif";
 import Typing from "react-typing-animation";
-import Aos from "aos";
-import "aos/dist/aos.css";
+
 
 function HomePage() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
 
   const AnimatedTyping = () => (
     <Typing>
@@ -27,13 +23,6 @@ function HomePage() {
           <img class="col-5" src={homegif} />
         </div>
       </StyleRoot>
-
-      //PLACEHOLDER
-      <div>
-        <p data-aos="fade-up">1</p>
-        <p>2</p>
-        <p>3</p>
-      </div>
     </>
   );
 }
--- src/Layout/Projects.js
+++ src/Layout/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleRoot } from "radium";
 import animationStyles from "../Components/animationStyles";
 import flashcards from "../imgs/flashcards.png";
@@ -8,149 +8,218 @@ import pomodoro from "../imgs/pomodoro.PNG";
 import grubdash from "../imgs/GrubDash.png";
 import "../App.css";
 import linkedButton from "../Components/buttonLink";
+import resSys from "../imgs/resres-system-thumbnail.png";
 
 function Projects() {
-  const [frontIsShowing, setFrontIsShowing] = useState(true);
+  // const [frontIsShowing, setFrontIsShowing] = useState(true);
 
-  const handleToggle = () => {
-    if (frontIsShowing === true) {
-      setFrontIsShowing(false);
-    } else {
-      setFrontIsShowing(true);
-    }
-  };
+  // const handleToggle = () => {
+  //   if (frontIsShowing === true) {
+  //     setFrontIsShowing(false);
+  //   } else {
+  //     setFrontIsShowing(true);
+  //   }
+  // };
 
   const inProgressList = () => {
     return (
       <>
-        <h4>Projects in Progress</h4>
+        {/* <h4>Projects in Progress</h4>
         <ul>
           <li>Periodic Tables: Restaurant Reservation System</li>
-        </ul>
+        </ul> */}
       </>
     );
   };
 
   const frontEndList = () => {
     return (
-      <div className="row">
-        <div className="col-lg-4 col-md-4 col-sm-12 my-3">
-          <img className="col-12 stack-images" src={flashcards} />
-          <h5 className="thumbnail-text">Flashcard-o-Matic</h5>
-          {linkedButton(
-            "https://github.com/ttran6943/Flashcard-O-Matic",
-            "GitHub Link"
-          )}
-        </div>
-        <div className="col-lg-4 col-md-4 col-sm-12 my-3">
-          <img className="col-12 stack-images" src={thinkfulCupid} />
-          <h5 className="thumbnail-text">ThinkfulCupid</h5>
-          <div className="row">
-            <div className="col mx-3 my-3">
-              {linkedButton(
-                "https://github.com/ttran6943/thinkfulCupid",
-                "GitHub Link"
-              )}
-            </div>
-            <div className="col mx-3 my-3">
-              {linkedButton(
-                "https://ttran6943.github.io/thinkfulCupid/",
-                "Live Demo"
-              )}
+      <>
+        <h5>Front-End Projects</h5>
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+            <img
+              className="col-12 stack-images"
+              alt="flashcard app"
+              src={flashcards}
+            />
+            <h5 className="thumbnail-text">Flashcard-o-Matic</h5>
+            {linkedButton(
+              "https://github.com/ttran6943/Flashcard-O-Matic",
+              "GitHub Link"
+            )}
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+            <img
+              className="col-12 stack-images toShadow"
+              alt="thinkfulCupid"
+              src={thinkfulCupid}
+            />
+            <h5 className="thumbnail-text my-3">ThinkfulCupid</h5>
+            <div className="row">
+              <div className="col mx-3 my-3">
+                {linkedButton(
+                  "https://github.com/ttran6943/thinkfulCupid",
+                  "GitHub Link"
+                )}
+              </div>
+              <div className="col mx-3 my-3">
+                {linkedButton(
+                  "https://ttran6943.github.io/thinkfulCupid/",
+                  "Live Demo"
+                )}
+              </div>
             </div>
           </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+            <img
+              className="col-12 stack-images toShadow"
+              alt="pomodoro app"
+              src={pomodoro}
+            />
+            <h5 className="thumbnail-text my-3">Pomodoro Timer</h5>
+            {linkedButton(
+              "https://github.com/ttran6943/Pomodoro-Timer",
+              "GitHub Link"
+            )}
+          </div>
         </div>
-        <div className="col-lg-4 col-md-4 col-sm-12 my-3">
-          <img className="col-12 stack-images" src={pomodoro} />
-          <h5 className="thumbnail-text">Pomodoro Timer</h5>
-          {linkedButton(
-            "https://github.com/ttran6943/Pomodoro-Timer",
-            "GitHub Link"
-          )}
-        </div>
-      </div>
+        <hr />
+      </>
     );
   };
 
   const backEndList = () => {
     return (
-      <div className="row backend">
-        <div className="col-lg-4 col-md-4 col-sm-12 my-3">
-          <img className="col-12 stack-images" src={movies} />
-          <h5 className="thumbnail-text">WeLoveMovies</h5>
-          <div className="col mx-3">
-            {linkedButton(
-              "https://ancient-temple-59879.herokuapp.com/",
-              "Live Demo"
-            )}
+      <>
+        <div>
+          <h5>Back-End Projects</h5>
+          <div className="row backend">
+            <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+              <img
+                className="col-12 stack-images"
+                alt="weLoveMovies app"
+                src={movies}
+              />
+              <h5 className="thumbnail-text">WeLoveMovies</h5>
+              <div className="col mx-3">
+                {linkedButton(
+                  "https://ancient-temple-59879.herokuapp.com/",
+                  "Live Demo"
+                )}
+              </div>
+              <div className="col my-3">
+                {linkedButton(
+                  "https://github.com/ttran6943/WeLoveMovies-back-end",
+                  "Github Link(Back-End)"
+                )}{" "}
+              </div>
+              <div className="col my-3">
+                {linkedButton(
+                  "https://github.com/ttran6943/WeLoveMovies-front-end",
+                  "Github Link(Front-End)"
+                )}
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+              <img
+                className="col-12 stack-images"
+                alt="grubdash project"
+                src={grubdash}
+              />
+              <h5 className="thumbnail-text">GrubDash</h5>
+              {linkedButton(
+                "https://github.com/ttran6943/thinkful-grubdash",
+                "GitHub Link"
+              )}
+            </div>
           </div>
-          <div className="col my-3">
-            {linkedButton(
-              "https://github.com/ttran6943/WeLoveMovies-back-end",
-              "Github Link(Back-End)"
-            )}{" "}
-          </div>
-          <div className="col my-3">
-            {linkedButton(
-              "https://github.com/ttran6943/WeLoveMovies-front-end",
-              "Github Link(Front-End)"
-            )}
+          <hr />
+        </div>
+      </>
+    );
+  };
+  //https://restaurant-res-sys-client.herokuapp.com/
+  const fullStackList = () => {
+    return (
+      <>
+        <h5>Full-Stack Projects</h5>
+        <div className="row backend">
+          <div className="col-lg-4 col-md-4 col-sm-12 my-3">
+            <img
+              className="col-12 stack-images toShadow"
+              alt="restaurant-reservation-system"
+              src={resSys}
+            />
+            <h5 className="thumbnail-text my-3">
+              Periodic Tables: Restaurant Reservation System
+            </h5>
+            <div className="row">
+              <div className="col">
+                {linkedButton(
+                  "https://restaurant-res-sys-client.herokuapp.com/",
+                  "Live Demo"
+                )}
+              </div>
+              <div className="col mx-3">
+                {linkedButton(
+                  "https://github.com/ttran6943/Periodic-Tables---Res-System",
+                  "GitHub Link"
+                )}
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-4 col-sm-12 my-3">
-          <img className="col-12 stack-images" src={grubdash} />
-          <h5 className="thumbnail-text">GrubDash</h5>
-          {linkedButton(
-            "https://github.com/ttran6943/thinkful-grubdash",
-            "GitHub Link"
-          )}
-        </div>
-      </div>
+      </>
     );
   };
 
-  const toggleButtons = () => {
-    if (frontIsShowing === true) {
-      return (
-        <div class="btn-group mb-3" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            onClick={handleToggle}
-          >
-            Front-End
-          </button>
-          <button type="button" class="btn btn-light" onClick={handleToggle}>
-            Back-End
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div class="btn-group mb-3" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-light" onClick={handleToggle}>
-            Front-End
-          </button>
-          <button
-            type="button"
-            class="btn btn-secondary"
-            onClick={handleToggle}
-          >
-            Back-End
-          </button>
-        </div>
-      );
-    }
-  };
+  // const toggleButtons = () => {
+  //   if (frontIsShowing === true) {
+  //     return (
+  //       <div class="btn-group mb-3" role="group" aria-label="Basic example">
+  //         <button
+  //           type="button"
+  //           class="btn btn-secondary"
+  //           onClick={handleToggle}
+  //         >
+  //           Front-End
+  //         </button>
+  //         <button type="button" class="btn btn-light" onClick={handleToggle}>
+  //           Back-End
+  //         </button>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div class="btn-group mb-3" role="group" aria-label="Basic example">
+  //         <button type="button" class="btn btn-light" onClick={handleToggle}>
+  //           Front-End
+  //         </button>
+  //         <button
+  //           type="button"
+  //           class="btn btn-secondary"
+  //           onClick={handleToggle}
+  //         >
+  //           Back-End
+  //         </button>
+  //       </div>
+  //     );
+  //   }
+  // };
 
   return (
     <StyleRoot>
       <div className="projects-page mx-4 my-3" style={animationStyles.fadeIn}>
         <h3>Projects</h3>
+
         {inProgressList()}
         <div className="text-center">
-          {toggleButtons()}
-          {frontIsShowing == true ? frontEndList() : backEndList()}
+          {/* {toggleButtons()} */}
+          {/* {frontIsShowing == true ? frontEndList() : backEndList()} */}
+          {frontEndList()}
+          {backEndList()}
+          {fullStackList()}
         </div>
       </div>
     </StyleRoot>
